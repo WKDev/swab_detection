5b7e4e31ba36cd1de82ecd813f9854ad5eb45f3b
wkdev <chanhyukman@gmail.com>
2024-09-24 22:09:22 +0900

    0924-5

--- main.py
+++ main.py
@@ -8,6 +8,17 @@ from matplotlib import pyplot as plt
 from utils.estimator import estimate_cnt
 from utils.misc import Perf,Text
 
+# TODO:
+# 1. 면봉케이스 바깥쪽 검출된 원 제거하기,  - 손
+# 2. 검정부분 헷갈리지 않게 아예 0으로 만들기- 송
+# 3. 원의 중심점이 까만 영역에 있으면 제거하기 - 윤
+# 4. 머신러닝 gridsearch 만들기 - 손
+
+# presentation - 손찬혁
+# 1. 반전 했더니 잘 된다.
+# 2. 허프 변환은 진한 색을 먼저 잡는다. 
+# 3. 이진화는 보조용이더라.(오탐 제거용)
+
 def update_value(value, config_key):
     if config_key in config:
         config[config_key][2] = value
@@ -36,6 +47,7 @@ config = {
     "gamma": [[0,100], 50, 50],       # range, default, current_value
     "min_radius": [[20,50], 30, 30],  # range, default, current_value
     "max_radius": [[20,50], 40, 40],  # range, default, current_value
+    "min_dist": [[50, 100], 75, 75],       # range, default, current_value
     "param1": [[0,100], 10, 10],      # range, default, current_value
     "param2": [[0,100], 20, 20]       # range, default, current_value
 }
@@ -43,7 +55,7 @@ binary_config = {
     "threshold_min": [[0, 255], 0, 0],
     "threshold_max": [[0, 255], 255, 255],
     "max_value": [[0, 255], 255, 255],
-    "block_size": [[3, 11], 11, 11],
+    "block_size": [[3, 100], 11, 11],
     "c": [[0, 255], 2, 2]
 }
 
@@ -52,13 +64,12 @@ morphological_config = {
     "iterations": [[1, 10], 1, 1]
 }
 
-
 # Global variable to store the result of HoughCircles
 hough_result = None
 
-def hough_circles_operation(image, param1, param2, min_radius, max_radius):
+def hough_circles_operation(image, min_dist,param1, param2, min_radius, max_radius):
     global hough_result
-    hough_result = cv2.HoughCircles(image, cv2.HOUGH_GRADIENT, 1, 75,
+    hough_result = cv2.HoughCircles(image, cv2.HOUGH_GRADIENT, 1, minDist=min_dist,
                                     param1=param1, param2=param2, 
                                     minRadius=min_radius, maxRadius=max_radius)
 
@@ -86,6 +97,7 @@ while True:
     gamma = config["gamma"][2]
     min_radius = config["min_radius"][2]
     max_radius = config["max_radius"][2]
+    min_dist = config["min_dist"][2]
     param1 = config["param1"][2]
     param2 = config["param2"][2]
 
@@ -112,25 +124,30 @@ while True:
     clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
     clahed_gray = clahe.apply(src)
 
+    adjusted = cv2.convertScaleAbs(clahed_gray, alpha=contrast/50, beta=brightness-50)
+    adjusted = cv2.addWeighted(adjusted, gamma/50, np.zeros(src.shape, src.dtype), 0, 0)
 
+    processed_imgs.update({"adjusted": adjusted})
 
     try:
-        adaptive_thresh_image = cv2.adaptiveThreshold(clahed_gray, max_value, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, block_size, c)
-        thresh_bin_image = cv2.threshold(clahed_gray, threshold_min, threshold_max, cv2.THRESH_BINARY)[1]
-        thresh_otsu_image = cv2.threshold(clahed_gray, threshold_min, threshold_max, cv2.THRESH_OTSU)[1]
+        adaptive_thresh_image = cv2.adaptiveThreshold(adjusted, max_value, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, block_size, c)
+        thresh_bin_image = cv2.threshold(adjusted, threshold_min, threshold_max, cv2.THRESH_BINARY)[1]
+        thresh_otsu_image = cv2.threshold(adjusted, threshold_min, threshold_max, cv2.THRESH_OTSU)[1]
 
         processed_imgs.update({"createCLAHE": clahed_gray})
         processed_imgs.update({"adaptiveThreshold": adaptive_thresh_image})
         processed_imgs.update({"threshold_binary": thresh_bin_image})
         processed_imgs.update({"threshold_otsu": thresh_otsu_image})
 
-        bin_image = adaptive_thresh_image
+        bin_image = adjusted
 
         # morphological operations
         kernel = np.ones((kernel_size, kernel_size), np.uint8)
         for i in range(iterations):
-            bin_image = cv2.morphologyEx(bin_image, cv2.MORPH_CLOSE, kernel)
-            bin_image = cv2.morphologyEx(bin_image, cv2.MORPH_OPEN, kernel)
+            bin_image = cv2.morphologyEx(adjusted, cv2.MORPH_CLOSE, kernel)
+            bin_image = cv2.morphologyEx(adjusted, cv2.MORPH_OPEN, kernel)
+
+        processed_imgs.update({"morphology": bin_image})
 
     except Exception as e:
         print(f"Error: {e}")
@@ -140,15 +157,14 @@ while True:
         contrast = 1
     if gamma == 0:
         gamma = 1
+
+
     
-    adjusted = cv2.convertScaleAbs(src, alpha=contrast/50, beta=brightness-50)
-    adjusted = cv2.addWeighted(adjusted, gamma/50, np.zeros(src.shape, src.dtype), 0, 0)
 
-    processed_imgs.update({"adjusted": adjusted})
-
+    inversed = cv2.bitwise_not(bin_image)
     # Start the HoughCircles operation in a separate thread
     hough_thread = threading.Thread(target=hough_circles_operation, 
-                                    args=(adjusted, param1, param2, min_radius, max_radius))
+                                    args=(inversed, min_dist, param1, param2, min_radius, max_radius))
     hough_thread.start()
 
     # Wait for the thread to complete or timeout
@@ -187,13 +203,11 @@ while True:
         if len(v.shape) == 2:
             processed_imgs[k] = cv2.cvtColor(v, cv2.COLOR_GRAY2BGR)
 
-        processed_imgs[k] = cv2.putText(processed_imgs[k], k, (50, processed_imgs[k].shape[1]-100), cv2.FONT_HERSHEY_SIMPLEX, 5, (0, 255, 0), 10)
+        processed_imgs[k] = cv2.putText(processed_imgs[k], k, (50, processed_imgs[k].shape[1]-300), cv2.FONT_HERSHEY_SIMPLEX, 5, (0, 255, 0), 10)
         stacked.append(processed_imgs[k])
 
     stacked = cv2.hconcat(stacked)
 
-    
-    
     # pf.show_perf()
 
     cv2.imshow("image", stacked)
